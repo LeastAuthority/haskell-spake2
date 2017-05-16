9f4c9bc1c930ed48c68b76105d2f0fb980233a87
Jonathan Lange <jml@mumak.net>
2017-05-16 19:02:16 +0100

    Complete the documentation of SPAKE2

--- src/Crypto/Spake2.hs
+++ src/Crypto/Spake2.hs
@@ -34,9 +34,6 @@ like 'Crypto.ECC.Curve_X25519', and generally anything that implements the
 'Crypto.ECC.EllipticCurve' typeclass in a sane way. It is possible that this
 typeclass is insufficient.
 
-__TODO__: describe points and scalars in relation to this group.
-
-
 If the connection is asymmetric (e.g. if user A is a client and user B is a
 server), then they will also have:
 
@@ -59,13 +56,76 @@ This password is mapped to a /scalar/ in group \(G\), in a way that's mutually
 agreed to both parties. The means of mapping may be public, but the actual
 mapped value /must/ be secret.
 
+=== Definitions
+
+__NOTE__: This is jml's best understanding. It's likely to be wrong.
+
+[@scalar@]: a number between 0 and \(p\) (that is, in \(\mathbb{Z}_{p}\)).
+
+[@point@]: a member of the group, \(G\).
+
+[@addition@]: the binary operation on points in the group \(G\). Confusingly,
+literature often writes this using product notation.
+
+[@scalar multiplication@]: adding a point to itself a scalar number of times.
+Confusingly, this is often written as \(X^{n}\) where \(X\) is a point and
+\(n\) a scalar.
+
+By convention, the group operation is referred to as "addition".
 
 === Protocol
 
-One side, A, initiates the exchange. They draw a random point, \(X\) from the
-group.
+==== How we map the password to a scalar
 
-|-}
+TODO
+
+==== How we exchange information
+
+/This is derived from the paper linked above./
+
+One side, A, initiates the exchange. They draw a random scalar, \(x\), and
+matching point, \(X\), from the group. They then "blind" \(X\) by adding it to
+\(M\) multiplied by the password in scalar form. Call this \(X^{\star}\).
+
+\[X^{\star} \leftarrow X \cdot M^{pw}\]
+
+to the other side, side B.
+
+Side B does the same thing, except they use \(N\) instead of \(M\) to blind
+the result, and they call it \(Y\) instead of \(X\).
+
+\[Y^{\star} \leftarrow Y \cdot N^{pw}\]
+
+After side A receives \(Y^{\star}\), it calculates \(K_A\), which is the last
+missing input in calculating the session key.
+
+\[K_A \leftarrow (Y^{\star}/N^{pw})^x\]
+
+That is, \(K_A\) is \(Y^{\star}\) subtracted from \(N\) scalar multiplied by
+\(pw\), all of which is scalar multiplied by \(x\).
+
+Side B likewise calculates:
+
+\[K_B \leftarrow (X^{\star}/M^{pw})^y\]
+
+They then both figure out the session key:
+
+\[SK \leftarrow H(A, B, X^{\star}, Y^{\star}, K)\]
+
+Where side A uses \(K_A\) and side B uses \(K_B\).
+
+If both parties were honest and knew the password, the key will be the same on both sides.
+
+==== How python-spake2 works
+
+TODO
+
+Hopefully the same as what was written above. Open questions:
+
+- what's up with its obsession in padding things out to a certain number of bytes
+- are we sure we're using the same hash algorithm, "blinds" 
+
+-}
 
 module Crypto.Spake2
   ( something
