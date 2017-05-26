5cfb4d7321ac0bceb9e7bee2a0ff4c6efeca7a20
Jonathan Lange <jml@mumak.net>
2017-05-26 12:23:46 +0100

    Print quickcheck counter-examples on failure

--- tests/Groups.hs
+++ tests/Groups.hs
@@ -7,7 +7,7 @@ import Protolude hiding (group)
 import Crypto.Number.Serialize (i2osp)
 import Crypto.Error (CryptoFailable(..))
 import GHC.Base (String)
-import Test.QuickCheck (Gen, arbitrary, forAll, property)
+import Test.QuickCheck (Gen, (===), arbitrary, forAll, property)
 import Test.Tasty (TestTree)
 import Test.Tasty.Hspec (Spec, testSpec, describe, it)
 
@@ -39,44 +39,44 @@ groupProperties
   -> Spec
 groupProperties name group elements scalars = describe name $ do
   it "addition is associative" $ property $
-    forAll triples $ \(x, y, z) -> elementAdd group (elementAdd group x y) z == elementAdd group x (elementAdd group y z)
+    forAll triples $ \(x, y, z) -> elementAdd group (elementAdd group x y) z === elementAdd group x (elementAdd group y z)
 
   it "addition with inverse yields identity" $ property $
-    forAll elements $ \x -> elementAdd group x (elementNegate group x) == groupIdentity group
+    forAll elements $ \x -> elementAdd group x (elementNegate group x) === groupIdentity group
 
   it "double negative is no-op" $ property $
-    forAll elements $ \x -> elementNegate group (elementNegate group x) == x
+    forAll elements $ \x -> elementNegate group (elementNegate group x) === x
 
   it "subtraction is negated addition" $ property $
-    forAll pairs $ \(x, y) -> elementSubtract group x y == elementAdd group x (elementNegate group y)
+    forAll pairs $ \(x, y) -> elementSubtract group x y === elementAdd group x (elementNegate group y)
 
   it "right-hand addition with identity yields original" $ property $
-    forAll elements $ \x -> elementAdd group x (groupIdentity group) == x
+    forAll elements $ \x -> elementAdd group x (groupIdentity group) === x
 
   it "left-hand addition with identity yields original" $ property $
-    forAll elements $ \x -> elementAdd group (groupIdentity group) x == x
+    forAll elements $ \x -> elementAdd group (groupIdentity group) x === x
 
   it "element codec roundtrips" $ property $
     forAll elements $ \x -> let bytes = encodeElement group x :: ByteString
                             in decodeElement group bytes == CryptoPassed x
 
   it "scalar to integer roundtrips" $ property $
-    forAll scalars $ \n -> integerToScalar group (scalarToInteger group n) == n
+    forAll scalars $ \n -> integerToScalar group (scalarToInteger group n) === n
 
   it "integer to scalar conversion" $ property $
     -- Doesn't roundtrip per se, because negative integers (for example) get
     -- turned into scalars within the subgroup range, losing the original
     -- information.
-    \i -> integerToScalar group (scalarToInteger group (integerToScalar group i)) == integerToScalar group i
+    \i -> integerToScalar group (scalarToInteger group (integerToScalar group i)) === integerToScalar group i
 
   it "scalar multiply by 0 is identity" $ property $
-    forAll elements $ \x -> scalarMultiply group (integerToScalar group 0) x == groupIdentity group
+    forAll elements $ \x -> scalarMultiply group (integerToScalar group 0) x === groupIdentity group
 
   it "scalar multiply by 1 is original" $ property $
-    forAll elements $ \x -> scalarMultiply group (integerToScalar group 1) x == x
+    forAll elements $ \x -> scalarMultiply group (integerToScalar group 1) x === x
 
   it "scalar multiply by 2 is equivalent to addition" $ property $
-    forAll elements $ \x -> scalarMultiply group (integerToScalar group 2) x == elementAdd group x x
+    forAll elements $ \x -> scalarMultiply group (integerToScalar group 2) x === elementAdd group x x
 
   where
     pairs = do
