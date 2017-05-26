5884797eaa8aa5258e56956250ed338500458ae2
Jonathan Lange <jml@mumak.net>
2017-05-26 16:38:38 +0100

    Ed25519 implementation
    Based very heavily on the one in python-spake2

--- tests/Groups.hs
+++ tests/Groups.hs
@@ -4,40 +4,46 @@ module Groups (tests) where
 
 import Protolude hiding (group)
 
-import Crypto.Number.Serialize (i2osp)
 import Crypto.Error (CryptoFailable(..))
 import GHC.Base (String)
 import Test.QuickCheck (Gen, (===), arbitrary, forAll, property)
 import Test.Tasty (TestTree)
-import Test.Tasty.Hspec (Spec, testSpec, describe, it)
+import Test.Tasty.Hspec (Spec, testSpec, describe, it, shouldBe)
 
 import Crypto.Spake2.Group (Group(..))
-import Crypto.Spake2.Groups (IntegerAddition(..), IntegerGroup(..), i1024)
+import Crypto.Spake2.Groups
+  ( IntegerAddition(..)
+  , IntegerGroup(..)
+  , Ed25519(..)
+  , i1024)
+import qualified Crypto.Spake2.Groups.Ed25519 as Ed25519
+import qualified Crypto.Spake2.Groups.IntegerGroup as IntegerGroup
 
 tests :: IO TestTree
 tests = testSpec "Groups" $ do
-  groupProperties "integer addition modulo 7" (IntegerAddition 7) (makeElement (IntegerAddition 7)) (makeScalar 7)
-  groupProperties "integer group" i1024 (makeElement i1024) (makeScalar (subgroupOrder i1024))
+  groupProperties "integer addition modulo 7" (IntegerAddition 7) 1 (makeScalar 7)
+  groupProperties "integer group" i1024 (IntegerGroup.generator i1024) (makeScalar (subgroupOrder i1024))
+  groupProperties "Ed25519" Ed25519 Ed25519.generator (makeScalar Ed25519.l)
+
 
 makeScalar :: Integer -> Gen Integer
 makeScalar k = do
   i <- arbitrary
   pure $ i `mod` k
 
-makeElement :: Group group => group -> Gen (Element group)
-makeElement group = do
-  i <- arbitrary
-  let bytes = i2osp i :: ByteString
-  pure $ arbitraryElement group bytes
+makeElement :: Group group => group -> Gen (Scalar group) -> Element group -> Gen (Element group)
+makeElement group scalars base = do
+  scalar <- scalars
+  pure (scalarMultiply group scalar base)
 
 groupProperties
   :: (Group group, Eq (Element group), Eq (Scalar group), Show (Element group), Show (Scalar group))
   => String
   -> group
-  -> Gen (Element group)
+  -> Element group
   -> Gen (Scalar group)
   -> Spec
-groupProperties name group elements scalars = describe name $ do
+groupProperties name group base scalars = describe name $ do
   it "addition is associative" $ property $
     forAll triples $ \(x, y, z) -> elementAdd group (elementAdd group x y) z === elementAdd group x (elementAdd group y z)
 
@@ -46,6 +52,9 @@ groupProperties name group elements scalars = describe name $ do
 
   it "double negative is no-op" $ property $
     forAll elements $ \x -> elementNegate group (elementNegate group x) === x
+
+  it "identity is its own inverse" $
+    elementNegate group (groupIdentity group) `shouldBe` groupIdentity group
 
   it "subtraction is negated addition" $ property $
     forAll pairs $ \(x, y) -> elementSubtract group x y === elementAdd group x (elementNegate group y)
@@ -79,6 +88,8 @@ groupProperties name group elements scalars = describe name $ do
     forAll elements $ \x -> scalarMultiply group (integerToScalar group 2) x === elementAdd group x x
 
   where
+    elements = makeElement group scalars base
+
     pairs = do
       x <- elements
       y <- elements
